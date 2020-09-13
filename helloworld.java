174b2182d3f23c37da42f65f2f985f007e6a923d
root <root@ip-172-31-46-154.us-east-2.compute.internal>
2020-09-13 16:40:18 +0000

    changed the content

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps World!"); // Display the string.
+        System.out.println("Hello DevOps Class"); // Display the string.
     }
 }
